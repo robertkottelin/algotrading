7158d9d6235309955d496f1ea836703a51d2ddf3
Robert Kottelin <robert.kottelin@gmail.com>
2023-10-29 11:34:10 +0100

    updates

--- train_neural_network.py
+++ train_neural_network.py
@@ -4,27 +4,22 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+import matplotlib.pyplot as plt
 
 # Directory containing your CSV files
 data_dir = 'data/preppeddata/'
 
 # Function to load and preprocess data
-def load_and_preprocess_data(file_path):
-    df = pd.read_csv(file_path)
-    
-    # Assuming the 'Next_Higher' column is your target
-    X = df.drop('Next_Higher', axis=1)
-    y = df['Next_Higher']
-
-    # Standardize the features (important for neural network training)
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    # Split the data into training and testing sets
-    return train_test_split(X_scaled, y, test_size=0.1, random_state=42)
+def load_and_combine_data(data_directory):
+    combined_data = pd.DataFrame()
+    for filename in os.listdir(data_directory):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(data_directory, filename)
+            df = pd.read_csv(file_path)
+            combined_data = pd.concat([combined_data, df], ignore_index=True)
+    return combined_data
 
 # Define the neural network model
 def build_advanced_model(input_dim):
@@ -94,24 +89,67 @@ def build_very_large_model(input_dim):
 
     return model
 
-# Iterate over files and train a model for each
-for filename in os.listdir(data_dir):
-    if filename.endswith('.csv'):
-        file_path = os.path.join(data_dir, filename)
-        
-        # Load and preprocess the data from the file
-        X_train, X_test, y_train, y_test = load_and_preprocess_data(file_path)
+def plot_training_history(history):
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
 
-        # Build the model
-        model = build_advanced_model(X_train.shape[1])  # input_dim is the number of features
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
 
-        # Train the model
-        model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+    plt.tight_layout()
+    plt.show()
 
-        # After training, you might want to save the model, evaluate it, or further fine-tune it.
-        # Save model
-        model.save(f"models/model_for_{filename}.h5")  # or another appropriate path
+# Load and combine data
+combined_data = load_and_combine_data(data_dir)
 
-        # Evaluate model
-        loss, accuracy = model.evaluate(X_test, y_test)
-        print(f"Model for {filename} - Test Loss: {loss} - Test Accuracy: {accuracy}")
+# Assuming the 'Next_Higher' column is your target
+X = combined_data.drop('Next_Higher', axis=1)
+y = combined_data['Next_Higher']
+
+# Standardize the features (important for neural network training)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.1, random_state=42)
+
+# Prepare a learning rate scheduler
+lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.7, patience=3, min_lr=1e-4)
+
+# Set higher batch_size and epochs due to the availability of substantial computational resources
+batch_size = 512 # decrease? increase?
+epochs = 10 
+
+# Build the model (using the more complex version here)
+model = build_very_large_model(X_train.shape[1])  # input_dim is the number of features
+
+# Train the model with our new parameters
+history = model.fit(
+    X_train, 
+    y_train, 
+    epochs=epochs, 
+    batch_size=batch_size, 
+    validation_data=(X_test, y_test), 
+    callbacks=[lr_scheduler]  # Using learning rate scheduler
+)
+
+# Save model
+model.save("models/combined_model.h5")  # or another appropriate path
+
+# Evaluate model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f"Model for combined dataset - Test Loss: {loss} - Test Accuracy: {accuracy}")
+
+# Plot the training history
+plot_training_history(history)
