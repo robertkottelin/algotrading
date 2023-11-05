c0b1d194034bd0c282de7515cbe0e71b758d2071
Robert Kottelin <robert.kottelin@gmail.com>
2023-11-05 20:27:40 +0100

    updates

---
+++ model_training/RandomForestOptimization.py
@@ -0,0 +1,145 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import optuna
+import joblib
+
+# Directory containing your CSV files
+data_dir = os.path.join('data', 'preppedconcatdata', 'combined_data.csv')
+
+# Ensure the 'models' directory exists
+os.makedirs('models', exist_ok=True)
+LOG_FILE = "logs/optuna_trials_randomforest.log"
+
+def log_trial(trial, trial_result):
+    try:
+        with open(LOG_FILE, "a") as log_file:
+            log_message = f"Trial {trial.number} finished with value: {trial_result} and parameters: {trial.params}.\n"
+            log_file.write(log_message)
+    except Exception as e:
+        print(f"Error logging trial {trial.number}: {e}")
+
+# Load and preprocess data
+try:
+    df = pd.read_csv(data_dir)
+    # smaller dataset for testing
+    # df_sampled = df.sample(frac=0.1, random_state=42)
+    X = df.drop('Next_Higher', axis=1)
+    y = df['Next_Higher'].astype(int)
+
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(y)
+except Exception as e:
+    print(f"Failed to load and preprocess data: {e}")
+    exit(1)
+
+# Data splitting
+try:
+    # test_size_proportion = 0.01 * (len(df_sampled) / len(df))
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size_proportion, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
+except Exception as e:
+    print(f"Failed to split data: {e}")
+    exit(1)
+
+def objective(trial):
+    try:
+        print(f"Starting trial {trial.number}")
+        # Hyperparameters to be tuned by Optuna
+        n_estimators = trial.suggest_int('n_estimators', 100, 500)
+        max_depth = trial.suggest_int('max_depth', 10, 200)         
+        min_samples_split = trial.suggest_int('min_samples_split', 2, 200)
+        min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 50)
+        criterion = trial.suggest_categorical('criterion', ['gini', 'entropy'])
+
+        print(f"Trial {trial.number} parameters: "
+              f"n_estimators={n_estimators}, "
+              f"max_depth={max_depth}, "
+              f"min_samples_split={min_samples_split}, "
+              f"min_samples_leaf={min_samples_leaf}, "
+              f"criterion={criterion}")
+
+        # Create a random forest classifier
+        clf = RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            criterion=criterion,
+            random_state=42,
+            n_jobs=-1,
+            verbose=1      
+        )
+
+        # Train the model
+        clf.fit(X_train, y_train)
+
+        # Make predictions
+        predictions = clf.predict(X_test)
+
+        # Calculate accuracy
+        accuracy = accuracy_score(y_test, predictions)
+
+        print(f"Trial {trial.number} finished with accuracy: {accuracy}")
+
+        # Log the trial result
+        log_trial(trial, accuracy)
+
+        return accuracy
+    except Exception as e:
+        print(f"Error in trial {trial.number}: {e}")
+        return None
+
+# Optuna study
+try:
+    study = optuna.create_study(direction='maximize', pruner=optuna.pruners.MedianPruner())
+    study.optimize(objective, n_trials=50)
+except Exception as e:
+    print(f"Optuna optimization failed: {e}")
+    exit(1)
+
+# Best trial results
+best_trial = study.best_trial
+print(f"Best Accuracy: {best_trial.value}")
+print("Best hyperparameters: ", best_trial.params)
+
+# Log the best trial result
+log_trial(best_trial, best_trial.value)
+
+# Retrieve the best hyperparameters
+best_params = best_trial.params
+
+# Final model training with best hyperparameters
+try:
+    final_model = RandomForestClassifier(
+        n_estimators=best_params["n_estimators"],
+        max_depth=best_params["max_depth"],
+        min_samples_split=best_params["min_samples_split"],
+        min_samples_leaf=best_params["min_samples_leaf"],
+        criterion=best_params["criterion"],
+        random_state=42
+    )
+
+    final_model.fit(X_train, y_train)
+except Exception as e:
+    print(f"Failed to train the final model: {e}")
+    exit(1)
+
+# Save the final model
+try:
+    joblib.dump(final_model, os.path.join('models', 'final_optimized_random_forest.joblib'))
+except Exception as e:
+    print(f"Failed to save the final model: {e}")
+    exit(1)
+
+# Evaluate the final model
+try:
+    final_predictions = final_model.predict(X_test)
+    final_accuracy = accuracy_score(y_test, final_predictions)
+    print(f"Final Model - Test Accuracy: {final_accuracy}")
+except Exception as e:
+    print(f"Failed to evaluate the final model: {e}")
+    exit(1)
--- model_training/bayesian_optimization_SEQ.py
+++ model_training/bayesian_optimization_SEQ.py
@@ -1,5 +1,3 @@
-# !pip install optuna
-
 import os
 import pandas as pd
 import tensorflow as tf
@@ -15,8 +13,7 @@ import optuna
 import datetime
 
 # Directory containing your CSV files
-data_dir = 'data/preppedconcatdata/combined_data.csv'
-
+data_dir = '/content/drive/MyDrive/Algotrading/combined_data.csv'
 # Ensure the 'models' directory exists
 os.makedirs('models', exist_ok=True)
 LOG_FILE = "optuna_trials_DNN.log"
@@ -93,26 +90,26 @@ def plot_training_history(history):
 def objective(trial):
     # Hyperparameters to be tuned by Optuna
     lr = trial.suggest_float("lr", 1e-4, 1e-2, log=True)
-    batch_size = trial.suggest_categorical('batch_size', [256, 512, 1024])
-    epochs = trial.suggest_int('epochs', 10, 30)
+    batch_size = trial.suggest_categorical('batch_size', [64, 128, 256, 512, 1024])
+    epochs = trial.suggest_int('epochs', 100, 300)
 
     # Architecture hyperparameters
     num_layers = trial.suggest_int("num_layers", 4, 6)
     dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.2)
-    
+
     # ReduceLROnPlateau parameters
     reduction_factor = trial.suggest_float("reduction_factor", 0.8, 0.95)
-    patience = trial.suggest_int("patience", 2, 4)
+    patience = trial.suggest_int("patience", 2, 2)
     min_lr = trial.suggest_float("min_lr", 1e-5, 1e-2, log=True)
 
     # Model building
     model = Sequential()
-    model.add(Dense(trial.suggest_int('units_layer_0', 64, 1024), input_dim=X_train.shape[1], activation=trial.suggest_categorical('activation_layer_0', ['relu', 'tanh', 'sigmoid'])))
+    model.add(Dense(trial.suggest_int('units_layer_0', 256, 1024), input_dim=X_train.shape[1], activation=trial.suggest_categorical('activation_layer_0', ['relu', 'tanh', 'sigmoid'])))
     model.add(Dropout(rate=dropout_rate))
 
     # Adding variable layers based on the trial suggestion
     for i in range(1, num_layers):
-        model.add(Dense(trial.suggest_int(f'units_layer_{i}', 64, 1024), activation=trial.suggest_categorical(f'activation_layer_{i}', ['relu', 'tanh', 'sigmoid'])))
+        model.add(Dense(trial.suggest_int(f'units_layer_{i}', 256, 1024), activation=trial.suggest_categorical(f'activation_layer_{i}', ['relu', 'tanh', 'sigmoid'])))
         model.add(Dropout(rate=dropout_rate))
 
     model.add(Dense(1, activation='sigmoid'))
@@ -125,15 +122,15 @@ def objective(trial):
 
     # Callbacks
     lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=reduction_factor, patience=patience, min_lr=min_lr)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=4)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
     # Training the model
     history = model.fit(
-        X_train, 
-        y_train, 
-        epochs=epochs, 
-        batch_size=batch_size, 
-        validation_data=(X_test, y_test), 
+        X_train,
+        y_train,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(X_test, y_test),
         callbacks=[early_stopping, lr_scheduler],
         verbose=1
     )
@@ -175,11 +172,11 @@ best_params = study.best_trial.params
 # Final model training
 final_model = build_very_large_model(X_train.shape[1], best_lr)
 final_history = final_model.fit(
-    X_train, 
-    y_train, 
-    epochs=best_epochs, 
-    batch_size=best_batch_size, 
-    validation_data=(X_test, y_test), 
+    X_train,
+    y_train,
+    epochs=best_epochs,
+    batch_size=best_batch_size,
+    validation_data=(X_test, y_test),
     callbacks=[
         EarlyStopping(monitor='val_loss', patience=3),
         ReduceLROnPlateau(monitor='val_loss', factor=best_params["reduction_factor"], patience=best_params["patience"], min_lr=best_params["min_lr"])
@@ -194,3 +191,4 @@ print(f"Final Model - Test Loss: {final_loss} - Test Accuracy: {final_accuracy}"
 
 # Training history plot
 plot_training_history(final_history)
+
